f9035655a7d9ba2ef1d92fe87e6855a88703cd2f
manassaini <57766167+manassaini@users.noreply.github.com>
2019-11-21 10:03:58 -0700

    My sixth function

--- Week12-utility.py
+++ Week12-utility.py
@@ -34,3 +34,12 @@ def FindWordCount(list_, string):
     print (word_counter)
 
 def Union():
+
+#this works
+def Intersection(players, players2):
+    players = ["Mary", "Cody", "Joe", "Jill", "Xai", "Bodo"]
+    players2 = ["Melvin", "Martian", "Baka", "Xai", "Cody"]
+    for x in players:
+        for y in players2:
+            if x == y: 
+                print ("OUTPUT", x)
